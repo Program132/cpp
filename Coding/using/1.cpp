adc284f4cfdd9e6360fd3402c5e9ae39104042d6
beyptitloup <67320703+Program132@users.noreply.github.com>
2021-03-21 16:53:58 +0100

    using

---
+++ Coding/using/1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using str = std::string;
+
+int main()
+{
+    str myName = "Program";
+
+    std::cout << myName << std::endl;
+
+    return 0;
+}
---
+++ Coding/using/2.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using str = std::string;
+using ni = int;
+using nd = double;
+using c = char;
+
+int main()
+{
+    str myName = "Program";
+
+    std::cout << myName << std::endl;
+
+    ni number = 10;
+    nd note = 19.5;
+
+    std::cout << number << std::endl;
+    std::cout << note << std::endl;
+
+    c letter = 'a';
+
+    std::cout << letter << std::endl;
+
+    return 0;
+}
