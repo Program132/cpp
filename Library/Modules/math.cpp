81813e380c928fddb92d7e34d03c5ebe1813846b
beyptitloup <67320703+Program132@users.noreply.github.com>
2021-03-21 20:00:44 +0100

    module math

---
+++ Library/Modules/math.cpp
@@ -0,0 +1,24 @@
+#include "math.hpp"
+#include <iostream>
+
+using namespace N;
+
+double Math::add(double a, double b)
+{
+    return a + b;
+}
+
+double Math::remove(double a, double b)
+{
+    return a - b;
+}
+
+double Math::multi(double a, double b)
+{
+    return a * b;
+}
+
+double Math::divi(double a, double b)
+{
+    return a / b;
+}
---
+++ Library/Modules/math.hpp
@@ -0,0 +1,16 @@
+#ifndef MATH_HPP
+#define MATH_HPP
+
+namespace N
+{
+	class Math
+	{
+	public:
+		double add(double a, double b);
+		double remove(double a, double b);
+		double multi(double a, double b);
+		double divi(double a, double b);
+	};
+}
+
+#endif
